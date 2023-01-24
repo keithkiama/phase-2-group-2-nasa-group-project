b505ac08408988b666486f5b69d7235ecb21927d
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-01-24 10:24:51 +0300

    Featuring:     1. Changed the api script     2. Created Card and NavBar components     3. Renamed SolarSystem to homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,13 @@
 import '../styles/App.css';
+import NavBar from './NavBar';
+import Cards from './Cards';
 
 function App() {
   return (
-  <nav className="navbar navbar-expand-lg bg-light">
-    <div className="container-fluid">
-      <p className="navbar-brand">The Solar System</p>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <p className="nav-link" aria-current="page">Home</p>
-          </li>
-          <li className="nav-item">
-            <p className="nav-link">Explore the solar System</p>
-          </li>
-          <li className="nav-item">
-            <p className="nav-link">Learn about the solar system</p>
-          </li>
-          <li className="nav-item">
-            <p className="nav-link">History of space exploration</p>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <div className = "App">
+    <NavBar />
+    <Cards />
+  </div>
   );
 }
 
---
+++ src/components/Cards.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+  <div class="card">
+    <div class="card-body">
+      This is some text within a card body.
+    </div>
+  </div>
+  )
+};
+
+export default Cards
