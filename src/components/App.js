ad0cfa568d291337d6fb9006a1f61b2062512058
Daniel Njuguna <kagudante@gmail.com>
2023-01-24 15:10:19 +0300

    Featuring Merged branch with master

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../styles/App.css';
+import NavBar from './NavBar';
+import Cards from './Cards';
+import Learn from './Learn';
+
+function App() {
+  return (
+  <div className = "App">
+    <NavBar />
+    <Cards />
+    <Learn/>
+  </div>
+  );
+}
+
+export default App;
