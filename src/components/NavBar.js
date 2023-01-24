8527cd04964635c0bd1e1b956e37a67e265134c1
eve-atieno <atienoevelyne53@gmail.com>
2023-01-24 14:23:55 +0300

    added a searchbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,14 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg bg-light">
+        <nav className="navbar navbar-expand-lg bg-light ">
         <div className="container-fluid">
             <p className="navbar-brand">The Solar System</p>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
              <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <div className="collapse navbar-collapse float-right" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                     <p className="nav-link" aria-current="page">Home</p>
                 </li>
@@ -24,6 +24,10 @@ const NavBar = () => {
                     <p className="nav-link">History</p>
                 </li>
             </ul>
+            <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
             </div>
         </div>
         </nav>
