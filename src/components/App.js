15e2d41ab6736ba8c30a6aaf1d7becb1229fcb84
Daniel Njuguna <kagudante@gmail.com>
2023-01-24 14:58:08 +0300

    Added a component Learn.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import '../styles/App.css';
 import NavBar from './NavBar';
 import Cards from './Cards';
+import MoreAboutSolarSytem from './MoreAboutSolarSystem';
 
 function App() {
   return (
   <div className = "App">
     <NavBar />
     <Cards />
+    <MoreAboutSolarSytem/>
   </div>
   );
 }
