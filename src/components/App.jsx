1cfe40249605aa53e021832a4d8bc89d9490bb1b
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-01-25 10:00:47 +0300

    Fixed render error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,8 +14,6 @@ function App() {
     <NavBar />
     <Home />
     <FetchData />
-    <Learn />
-     <NavBar />
     <Routes>
        <Route exact path = "/" element = {<Home/>} />
        <Route exact path = "/" element = {<FetchData />}/>
