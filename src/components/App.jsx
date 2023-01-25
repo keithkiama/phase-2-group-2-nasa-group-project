17781dfd49c592ba0f5e56bdab1620b764cc060e
mukandalee <leonardmukanda254@gmail.com>
2023-01-25 06:09:33 +0300

    added a comment to test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from './NavBar';
 import FetchData from './FetchData';
 import Home from './Home';
+//this is the parent component to all child component
 
 function App() {
   return (
