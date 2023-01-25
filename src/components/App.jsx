03691280f4bcfd66e03c976fb3ee6e9f350cd3a3
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-01-25 23:49:50 +0300

    Edited components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,6 @@ import NavBar from './NavBar';
 import FetchData from './FetchData';
 import Home from './Home';
 import {Routes,Route} from 'react-router-dom';
-import Explore from './Explore';
 import History from './History';
 import Learn from './Learn';
 
@@ -14,12 +13,14 @@ function App() {
     <NavBar />
     <Home />
     <FetchData />
+    <History />
+    <Learn />
+
     <Routes>
        <Route exact path = "/" element = {<Home/>} />
-       <Route exact path = "/" element = {<FetchData />}/>
-       <Route exact path = "/Explore" element = {<Explore/>} />
-       <Route exact path = "/History" element = {<History/>} />
-       <Route exact path = "/Learn" element = {<Learn/>} />
+       <Route exact path = "FetchData" element = {<FetchData />}/>
+       <Route exact path = "History" element = {<History/>} />
+       <Route exact path = "Learn" element = {<Learn/>} />
     </Routes>
   </div>
   );
