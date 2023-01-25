f2efce71e3835900c496afe11adf5110fb5ca699
Daniel Njuguna <kagudante@gmail.com>
2023-01-25 09:21:29 +0300

    added link to Learn.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from './NavBar';
 import FetchData from './FetchData';
 import Home from './Home';
+import Learn from './Learn'
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
     <NavBar />
     <Home />
     <FetchData />
-
+    <Learn />
+    <button>Click here to Learn more about solar system</button>
   </div>
   );
 }
