94f7fce05652ee95e5f2d399d98af753b3be96f1
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-01-25 11:42:45 +0300

    Fixed background Image repeat on explore page

--- src/components/FetchData.jsx
+++ src/components/FetchData.jsx
@@ -18,7 +18,7 @@ const FetchData = () => {
 
   return (
     <>
-    <div style={{backgroundImage:'url("https://images.unsplash.com/photo-1532721344391-eaf75d3ab37f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")', backgroundPosition:'cover'}}>
+    <div style={{backgroundImage:'url("https://images.unsplash.com/photo-1532721344391-eaf75d3ab37f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")', backgroundPosition:'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '100%'}}>
     <h1 className='card-header' style={{textAlign: 'center', color:'#FFF',paddingTop:'10px'}}>The Bodies in our Solar System</h1>
     {cardLoop}
     </div>
