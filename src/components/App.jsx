d9fda0fddb00028ab9d0df4c3ec16236b02d6a79
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-01-26 00:30:11 +0300

    Changed component call in Explore

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
  <div className = "App">
     <NavBar />
-    {/* <FetchData /> */}
+    <FetchData />
     {/* <Learn /> */}
     <Routes>
        <Route exact path = "/" element = {<Home/>} />
