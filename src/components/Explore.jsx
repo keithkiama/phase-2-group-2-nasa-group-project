5d74fd7c9e8d5bdbcf4d781ee09d644e13f44bbf
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-01-26 16:09:37 +0300

    run the npm run build command

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -22,7 +22,7 @@ const Explore = () => {
     <Search Search={search} setSearch={setSearch} />
     <div style={{backgroundImage:'url("https://images.unsplash.com/photo-1532721344391-eaf75d3ab37f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")', backgroundPosition:'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', height: '100%'}}>
     <h1 className='card-header' style={{textAlign: 'center', color:'#FFF',paddingTop:'10px'}}>The Bodies in our Solar System</h1>
-      {cards.map(card =>{
+      {filtered.map(card =>{
         return(
           <Card key={card.id} id={card.id} image={card.image} name={card.name} description={card.description} />
         ) 
