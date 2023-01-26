4a6c0657622784bfc331570ecee84dcd9b65346d
mukandalee <leonardmukanda254@gmail.com>
2023-01-26 07:42:23 +0300

    completed history component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+
 const Home = () => {
   return (
     <div>
-      
+      <div>
+         <h1 style={{}} > Welcome To The Milky Way</h1>
+         <img src='../images/MilkyWay.jpg'/>
+      </div>
     </div>
   )
 }
