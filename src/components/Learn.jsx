1c8cb1f364337066b8ffbca384a72bbbc84935e8
Keith Kiama Maina <keithkiama2003@gmail.com>
2023-01-27 10:44:24 +0300

    Added untracked image

--- src/components/Learn.jsx
+++ src/components/Learn.jsx
@@ -4,7 +4,7 @@ const Learn = () => {
   return (
     <div style={{height:'100%',backgroundImage:'url("https://images.unsplash.com/photo-1532721344391-eaf75d3ab37f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")'}}>
       <h1 style={{textAlign:'center', color:'white',padding:'0 0 10px 0', fontSize:'15mm'}}>Learn more about the solar system</h1>
-      <img  style={{backgroundPosition: 'centre', backgroundRepeat: 'no-repeat',backgroundSize: 'center', maxWidth: '29cm',justifyContent:'center',paddingLeft:'400px'}} src='../../images/Solarsystem.jpg' alt=''/>
+      <img  style={{backgroundPosition: 'centre', backgroundRepeat: 'no-repeat',backgroundSize: 'center', maxWidth: '29cm',justifyContent:'center',paddingLeft:'400px'}} src='../../images/SolarSystem.jpg' alt=''/>
       <h2 style={{textAlign:'center', color:'white'}}>Why is it called the solar system?</h2>
         <p style={{textAlign:'center', color:'white',fontSize: '8mm', backgroundColor: 'black'}}>There are many planetary systems like ours in the universe, with planets orbiting a host star. Our planetary system is called “the solar system” because we use the word “solar” to describe things related to our star, after the Latin word for Sun, "solis."</p>
         <h1 className="card-text" style={{color: '#FFF', fontSize: '10mm',marginBottom: '10px',textAlign:'center'}}>What Are The Nine Planets?</h1>
