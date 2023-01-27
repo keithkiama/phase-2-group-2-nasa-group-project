38688c11c8b7ebc03582bfc90875758a18c4547d
eve-atieno <atienoevelyne53@gmail.com>
2023-01-27 23:25:10 +0300

    updated ViewData component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,33 +1,31 @@
-import React ,{useState}from 'react'
+import React, { useState, useEffect } from 'react'
 import ViewData from './ViewData'
 
 const Card = ({name, image, description}) => {
-  const [viewData,setViewData] = useState([])
+const [viewData,setViewData] = useState([])
 
-   
-      fetch("http://localhost:5000/solar-system")
-      .then(res=>res.json())
-      .then(data=>setViewData(data))
-      console.log(viewData);
-  
-  
-  return (
-    <>
-      <div className="card-deck col-4" style= {{display : 'inline-grid', paddingLeft: '45px'}}>
+useEffect(() => {
+fetch("http://localhost:5000/solar-system")
+.then(res => res.json())
+.then(data => setViewData(data))
+}, [])
 
-        <div className="card" style={{width :'25rem', margin : '15px 10px 25px 5px',backgroundColor : '#323233',borderColor:'#FFF'}}>
-          <img src={image} style={{ backgroundSize : 'cover', backgroundPosition: 'centre', height: '50vh',width: '100%'}} className="card-img-top" alt=""/>
+return (
+<>
+<div className="card-deck col-4" style={{ display: 'inline-grid', paddingLeft: '45px' }}>
+<div className="card" style={{ width: '25rem', margin: '15px 10px 25px 5px', backgroundColor: '#323233', borderColor: '#FFF' }}>
+<img src={image} style={{ backgroundSize: 'cover', backgroundPosition: 'center', height: '50vh', width: '100%' }} className="card-img-top" alt="" />
 
-          <div className="card-body">
-            <h3 className="card-title" style={{color: '#FFF'}}>{name}</h3>
-            <p className="card-text" style={{color: '#FFF'}}>{description}</p>
+      <div className="card-body">
+        <h3 className="card-title" style={{ color: '#FFF' }}>{name}</h3>
+        <p className="card-text" style={{ color: '#FFF' }}>{description}</p>
 
-            {viewData.length > 0 && <ViewData viewData={viewData}/> }
-          </div>
-        </div>
+        {viewData.length > 0 && <ViewData viewData={viewData} />}
       </div>
-  </>
-  )
-  }
+    </div>
+  </div>
+</>
+)
+}
 
 export default Card;
--- src/components/ViewData.jsx
+++ src/components/ViewData.jsx
@@ -2,7 +2,47 @@ import React,{useState} from 'react'
 
 function ViewData({viewData}) {
         const [isEditing, setIsEditing] = useState(false);
-       
+       const [inputName,setInputName] = useState("");
+       const [inputComment,setInpuComment] = useState("");
+
+       const [like, setLike] = useState(0)
+    const [liked, setLiked] = useState(false)
+    const [color, setColor] = useState("red")
+
+    function handleClick(){
+        setLike(like + 1)
+        setLiked(!liked)
+        if (liked){
+            setColor("red")
+        } else {
+            setColor("green")
+        }
+    }
+       const handleName = (e) =>{
+        setInputName(e.target.value);
+       }
+       const handleComment = (e) =>{
+        setInpuComment(e.target.value);
+       }
+       const onSubmit = (e) =>{
+        e.preventDefault();
+        const isData ={
+            name:inputName,
+            comment:inputComment
+        }
+        setInputName("");
+        setInpuComment("");
+
+        fetch("http://localhost:5000/comments",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+            
+        },
+        body: JSON.stringify(isData),
+    })
+}
+    
         
         const handleEditClick = () => {
             setIsEditing(true);
@@ -18,26 +58,39 @@ function ViewData({viewData}) {
     <div>
       {isEditing ? (
         <>
-        <br /> <br />
-        <ul>
-        <li>{viewData.galaxy}</li>
-        <li>{viewData.equatorCircumference}</li>
-        <li>{viewData.radius}</li>
-        <li>{viewData.averageDistanceFromSun}</li>
-        <li>{viewData.surfaceTemperature}</li>
-        <li>{viewData.dayLength}</li>
-        <li>{viewData.yearLength}</li>
-        <li>{viewData.averageorbitalSpeed}</li>
-        <li>{viewData.moons}</li>
-        <li>{viewData.planetType}</li>
-      </ul>
-         
+    <form onSubmit={onSubmit}>
+  <div class="mb-3">
+    <input 
+    value={inputName}
+
+
+    onChange={handleName}
+     type="text" 
+     placeholder='Enter Your Name'
+      class="form-control" 
+      id="exampleInputName"
+         aria-describedby="emailHelp"/>
+ </div>
+  <div class="mb-3">
+    <input 
+    value={inputComment}
+    onChange={handleComment}
+    type="text" 
+    class="form-control" 
+    placeholder='comment...' 
+    id="comment"/>
+  </div>
+  <button className="like-button" onClick={handleClick} style={{backgroundColor: color}}>{like} Likes</button>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
 
           
           <button onClick={handleSaveClick}>Save</button>
           <br />
         </>
       ) : (
+        
         <button style={{width:'60px', height:'40px'}}  className="btn btn-dark text-white mx-3" onClick={handleEditClick}>view</button>
       )}
     </div>
